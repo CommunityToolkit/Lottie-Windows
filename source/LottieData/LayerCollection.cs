709779c3ff62a2bd673626bdcd0a8dc813175156
Simeon Cran <simeonc@microsoft.com>
2019-01-03 16:39:57 -0800

    Be more resilient to Lottie layers with invalid indices.
    The Index of a Lottie layer is supposed to indicate its relative drawing order (layers are drawn from highest to lowest), as well as to allow it to be
    referenced as the parent transform of another layer.
    
    I've recently seen a Lottie with 2 layers having the same index. This was causing us to crash.
    
    This change fixes that crash by assuming that when there are 2 or more layers with the same index that they are in the Lottie file in drawing order,
    and that that index will never be used for transform inheritance. If they're not in drawing order then I don't know how any renderer would figure out how to draw them.
    If the index is used to reference a parent then I don't know how any renderer would figure out which layer to return. So I think this behavior is reasonable
    and definitely preferable to crashing.

--- source/LottieData/LayerCollection.cs
+++ source/LottieData/LayerCollection.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -15,18 +16,27 @@ namespace Microsoft.Toolkit.Uwp.UI.Lottie.LottieData
 #endif
     sealed class LayerCollection
     {
-        readonly Dictionary<int, Layer> _layersById;
+        readonly Layer[] _layers;
 
         public LayerCollection(IEnumerable<Layer> layers)
         {
-            _layersById = layers.ToDictionary(layer => layer.Index);
+            // The Index value is supposed to determine the drawing order (highest number
+            // is drawn first) and should be unique within a LayerCollection, however I've
+            // seen cases where 2 layers have the same index.
+            // In order to be resilient to non-unique indices, sort by index using a stable
+            // sort so that the layers with non-unique indices remain in the order they
+            // were specified in (this assumes that the specified order is actually
+            // the drawing order).
+            // If a request is made to find a layer with a non-unique index, return null.
+            // OrderBy is a stable sort, so non-unique keys will remain in their original order.
+            _layers = layers.OrderByDescending(layer => layer.Index).ToArray();
         }
 
         /// <summary>
         /// Returns the <see cref="Layer"/>s in the <see cref="LayerCollection"/> in
         /// painting order.
         /// </summary>
-        public IEnumerable<Layer> GetLayersBottomToTop() => _layersById.Values.OrderByDescending(layer => layer.Index);
+        public IEnumerable<Layer> GetLayersBottomToTop() => _layers;
 
         /// <summary>
         /// Returns the <see cref="Layer"/> with the given id, or null if no matching <see cref="Layer"/> is found.
@@ -38,7 +48,47 @@ namespace Microsoft.Toolkit.Uwp.UI.Lottie.LottieData
                 return null;
             }
 
-            return _layersById.TryGetValue(id.Value, out var result) ? result : null;
+            return BinarySearch(id.Value);
+        }
+
+        Layer BinarySearch(int targetIndex)
+        {
+            int min = 0;
+            int max = _layers.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                ref var current = ref _layers[mid];
+                var currentIndex = current.Index;
+                if (targetIndex == currentIndex)
+                {
+                    // Check that the index is unique by looking at the indices
+                    // on either side. If the index is not unique, return null.
+                    if ((mid > 0 && _layers[mid - 1].Index == targetIndex) ||
+                        (mid < _layers.Length - 1 && _layers[mid + 1].Index == targetIndex))
+                    {
+                        // Index is not unique. We don't know which Layer is needed, so return null.
+                        return null;
+                    }
+                    else
+                    {
+                        return current;
+                    }
+                }
+                else if (targetIndex > currentIndex)
+                {
+                    // Look left.
+                    max = mid - 1;
+                }
+                else
+                {
+                    // Look right.
+                    min = mid + 1;
+                }
+            }
+
+            // Not found.
+            return null;
         }
     }
 }
