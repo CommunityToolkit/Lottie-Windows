8f256fbcb6bea94743315478b6106435f99bc1fd
Sohum Chatterjee <sohumc@microsoft.com>
2019-02-06 11:56:59 -0800

    Added license headers

--- samples/LottieSamples/MainPage.xaml.cs
+++ samples/LottieSamples/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using LottieSamples.Scenarios;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using LottieSamples.Scenarios;
 using System;
 using System.Linq;
 using Windows.UI.Xaml;
