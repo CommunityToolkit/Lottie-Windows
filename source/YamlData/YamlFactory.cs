8c08d3b5f7c304085f9f53df45722beee184af68
Simeon Cran <simeonc@microsoft.com>
2019-05-03 19:35:52 -0700

    Move the YAML object model and writer into its own project so it can be shared by UIData.
    The YAML stuff was exclusive to LottieData before. But now I want to use Yaml for dumping WinCompData trees, thus this refactoring.

---
+++ source/YamlData/YamlFactory.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+namespace Microsoft.Toolkit.Uwp.UI.Lottie.YamlData
+{
+    /// <summary>
+    /// Base class for factories that create YAML representations of scalars.
+    /// </summary>
+    /// <remarks>Serializers should subclass this class and add their own methods for the
+    /// scalar types they wish to serialize.</remarks>
+#if PUBLIC_YamlData
+    public
+#endif
+    abstract class YamlFactory
+    {
+        protected YamlScalar Scalar(object value, string presentation) => new YamlScalar(value, presentation);
+    }
+}
---
+++ source/YamlData/YamlSequence.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Microsoft.Toolkit.Uwp.UI.Lottie.YamlData
+{
+    /// <summary>
+    /// A sequence of objects.
+    /// </summary>
+#if PUBLIC_YamlData
+    public
+#endif
+    sealed class YamlSequence : YamlObject, IEnumerable<YamlObject>
+    {
+        readonly List<YamlObject> _sequence = new List<YamlObject>();
+
+        /// <summary>
+        /// Appends the given object to the sequence.
+        /// </summary>
+        /// <param name="obj">The object to append.</param>
+        public void Add(YamlObject obj)
+        {
+            _sequence.Add(obj);
+        }
+
+        internal override YamlObjectKind Kind => YamlObjectKind.Sequence;
+
+        IEnumerator<YamlObject> IEnumerable<YamlObject>.GetEnumerator()
+        {
+            return ((IEnumerable<YamlObject>)_sequence).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<YamlObject>)this).GetEnumerator();
+    }
+}
