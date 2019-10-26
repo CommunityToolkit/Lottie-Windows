07fca1941dad5324861192c07b9134c3de6ccaac
Simeon <simeoncran@outlook.com>
2019-10-25 23:28:06 -0700

    Fixing copyright notice on new file.

--- source/UIData/CodeGen/LoadedImageSurfaceInfo.cs
+++ source/UIData/CodeGen/LoadedImageSurfaceInfo.cs
@@ -1,5 +1,6 @@
-﻿// Copyright(c) Microsoft Corporation.All rights reserved.
-// Licensed under the MIT License.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 using System;
 using Wmd = Microsoft.Toolkit.Uwp.UI.Lottie.WinUIXamlMediaData;
