c2fcd7923b15713189af7945e23101a039592346
Simeon Cran <simeonc@microsoft.com>
2019-12-04 13:51:29 -0800

    Remove some dead code.

--- source/Lottie/LottieVisualDiagnostics.cs
+++ source/Lottie/LottieVisualDiagnostics.cs
@@ -6,8 +6,6 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Toolkit.Uwp.UI.Lottie.GenericData;
 using Microsoft.Toolkit.Uwp.UI.Lottie.LottieData;
-using Microsoft.Toolkit.Uwp.UI.Lottie.UIData.CodeGen;
-using Microsoft.Toolkit.Uwp.UI.Lottie.WinCompData;
 
 namespace Microsoft.Toolkit.Uwp.UI.Lottie
 {
@@ -19,25 +17,6 @@ namespace Microsoft.Toolkit.Uwp.UI.Lottie
     public sealed class LottieVisualDiagnostics
     {
         public string FileName { get; internal set; } = string.Empty;
-
-        public string SuggestedFileName =>
-            string.IsNullOrWhiteSpace(FileName)
-                ? "MyComposition"
-                : System.IO.Path.GetFileNameWithoutExtension(FileName);
-
-        public string SuggestedClassName
-        {
-            get
-            {
-                string result = null;
-                if (LottieComposition != null)
-                {
-                    result = InstantiatorGeneratorBase.TrySynthesizeClassName(LottieComposition.Name);
-                }
-
-                return result ?? InstantiatorGeneratorBase.TrySynthesizeClassName(SuggestedFileName);
-            }
-        }
 
         public TimeSpan Duration => LottieComposition?.Duration ?? TimeSpan.Zero;
 
@@ -63,63 +42,11 @@ namespace Microsoft.Toolkit.Uwp.UI.Lottie
 
         public double LottieHeight => LottieComposition?.Height ?? 0;
 
-        public string LottieDetails => DescribeLottieComposition();
-
-        public string LottieVersion => LottieComposition?.Version.ToString() ?? string.Empty;
-
         /// <summary>
         /// Gets the options that were set on the <see cref="LottieVisualSource"/> when it
         /// produced this diagnostics object.
         /// </summary>
         public LottieVisualOptions Options { get; internal set; }
-
-        public string GenerateLottieXml()
-        {
-            if (LottieComposition == null) { return null; }
-            return LottieData.Serialization.LottieCompositionXmlSerializer.ToXml(LottieComposition).ToString();
-        }
-
-        public string GenerateWinCompXml()
-        {
-            return WinCompData.Tools.CompositionObjectXmlSerializer.ToXml(RootVisual).ToString();
-        }
-
-        public string GenerateCSharpCode()
-        {
-            if (LottieComposition == null) { return null; }
-
-            return CSharpInstantiatorGenerator.CreateFactoryCode(
-                SuggestedClassName,
-                new (CompositionObject, uint)[] { (RootVisual, RequiredUapVersion) },
-                SourceMetadata,
-                (float)LottieComposition.Width,
-                (float)LottieComposition.Height,
-                LottieComposition.Duration,
-                disableFieldOptimization: false).csText;
-        }
-
-        public void GenerateCxCode(string headerFileName, out string cppText, out string hText)
-        {
-            if (LottieComposition == null)
-            {
-                cppText = null;
-                hText = null;
-                return;
-            }
-
-            (string cppText, string hText, IEnumerable<Uri> assetList) generatedCode = CxInstantiatorGenerator.CreateFactoryCode(
-                SuggestedClassName,
-                new (CompositionObject, uint)[] { (RootVisual, RequiredUapVersion) },
-                SourceMetadata,
-                (float)LottieComposition.Width,
-                (float)LottieComposition.Height,
-                LottieComposition.Duration,
-                headerFileName,
-                disableFieldOptimization: false);
-
-            cppText = generatedCode.cppText;
-            hText = generatedCode.hText;
-        }
 
         public KeyValuePair<string, double>[] Markers { get; internal set; } = Array.Empty<KeyValuePair<string, double>>();
 
@@ -152,19 +79,6 @@ namespace Microsoft.Toolkit.Uwp.UI.Lottie
                 ValidationTime = ValidationTime,
                 TranslationIssues = TranslationIssues,
             };
-
-        // Creates a string that describes the Lottie.
-        string DescribeLottieComposition()
-        {
-            if (LottieComposition == null) { return null; }
-
-            var stats = new LottieData.Tools.Stats(LottieComposition);
-
-            return $"LottieVisualSource w={LottieComposition.Width} h={LottieComposition.Height} " +
-                $"layers: precomp={stats.PreCompLayerCount} solid={stats.SolidLayerCount} " +
-                $"image={stats.ImageLayerCount} null={stats.NullLayerCount} " +
-                $"shape={stats.ShapeLayerCount} text={stats.TextLayerCount}";
-        }
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
     }
 }
