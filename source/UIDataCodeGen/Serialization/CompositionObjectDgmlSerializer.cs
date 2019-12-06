75b6abd5eb5ee43b35bbc430a9acfbf4d24e0184
Simeon Cran <simeonc@microsoft.com>
2019-12-05 17:50:46 -0800

    Build break.

--- source/UIDataCodeGen/Serialization/CompositionObjectDgmlSerializer.cs
+++ source/UIDataCodeGen/Serialization/CompositionObjectDgmlSerializer.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
+using Microsoft.Toolkit.Uwp.UI.Lottie.UIData.Tools;
 using Microsoft.Toolkit.Uwp.UI.Lottie.WinCompData;
 using Microsoft.Toolkit.Uwp.UI.Lottie.WinCompData.Wui;
 
