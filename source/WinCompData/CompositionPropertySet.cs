087b4d87b4399719c4c22a3773922f0d002cd444
Simeon Cran <simeonc@microsoft.com>
2020-01-30 13:30:34 -0800

    Fix build break.

--- source/WinCompData/CompositionPropertySet.cs
+++ source/WinCompData/CompositionPropertySet.cs
@@ -4,7 +4,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Numerics;
 using Microsoft.Toolkit.Uwp.UI.Lottie.WinCompData.MetaData;
 using Microsoft.Toolkit.Uwp.UI.Lottie.WinCompData.Wui;
