878feaf102e25b9cd0e263b955b5e28cd41b82df
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-07-01 12:17:29 +0200

    Update player sample page

--- samples/LottieSamples/Scenarios/PlaybackPage.xaml.cs
+++ samples/LottieSamples/Scenarios/PlaybackPage.xaml.cs
@@ -2,44 +2,29 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Automation;
 using Windows.UI.Xaml.Controls;
 
 namespace LottieSamples.Scenarios
 {
     public sealed partial class PlaybackPage : Page
     {
+        private bool wasPaused = false;
+        
         public PlaybackPage()
         {
             this.InitializeComponent();
-        }
-
-        private void PlayButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Set forward playback rate.
-            // NOTE: This property is live, which means it takes effect even if the animation is playing.
-            Playback_Player.PlaybackRate = 1;
-            StartPlaying();
-        }
-
-        private void PauseButton_Checked(object sender, RoutedEventArgs e)
-        {
-            // Pause the animation, if playing.
-            // NOTE: Pausing does not cause PlayAsync to complete.
-            Playback_Player.Pause();
-        }
-
-        private void PauseButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            // Resume playing current animation.
-            Playback_Player.Resume();
         }
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
             // Stop the animation, which completes PlayAsync and resets to initial frame. 
             Playback_Player.Stop();
-            PauseButton.IsChecked = false;
+            Playback_Player.SetProgress(0);
+            SetIsPlayingIndicator(false);
+            wasPaused = false;
         }
 
         private void ReverseButton_Click(object sender, RoutedEventArgs e)
@@ -47,26 +32,63 @@ namespace LottieSamples.Scenarios
             // Set reverse playback rate.
             // NOTE: This property is live, which means it takes effect even if the animation is playing.
             Playback_Player.PlaybackRate = -1;
-            StartPlaying();
+            StartAnimation();
         }
 
-        private void StartPlaying()
+        private async void StartAnimation()
         {
-            // If already playing, keep playing till PlayAsync completes or is interrupted.
-            if (Playback_Player.IsPlaying && PauseButton.IsChecked == false)
+            if (!Playback_Player.IsPlaying)
             {
-                return;
+                // Play the animation at the currently specified playback rate.
+                Playback_Player.PlaybackRate = 1;
+                SetIsPlayingIndicator(true);
+                await Playback_Player.PlayAsync(fromProgress: 0, toProgress: 1, looped: false);
+                SetIsPlayingIndicator(false);
             }
+        }
 
-            // Resume playing the animation, if paused.
-            if (PauseButton.IsChecked == true)
+        private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Is the player playing and we did not get paused?
+            if (Playback_Player.IsPlaying && !wasPaused)
             {
-                PauseButton.IsChecked = false;
+                Playback_Player.Pause();
+                SetIsPlayingIndicator(false);
+                wasPaused = true;
             }
             else
             {
-                // Play the animation at the currently specified playback rate.
-                _ = Playback_Player.PlayAsync(fromProgress: 0, toProgress: 1, looped: false);
+                // Either not playing or paused
+                // If paused, resume and set paused to false
+                if (wasPaused)
+                {
+                    Playback_Player.Resume();
+                    SetIsPlayingIndicator(true);
+                }
+                // Not playing, start animation now
+                else
+                {
+                    StartAnimation();
+                }
+                wasPaused = false;
+            }
+        }
+
+        private void SetIsPlayingIndicator(bool isPlaying)
+        {
+            if (isPlaying)
+            {
+                PlayIcon.Visibility = Visibility.Collapsed;
+                PauseIcon.Visibility = Visibility.Visible;
+                ToolTipService.SetToolTip(PlayPauseButton, "Pause");
+                PlayPauseButton.SetValue(AutomationProperties.NameProperty, "Pause");
+            }
+            else
+            {
+                PlayIcon.Visibility = Visibility.Visible;
+                PauseIcon.Visibility = Visibility.Collapsed;
+                ToolTipService.SetToolTip(PlayPauseButton, "Play");
+                PlayPauseButton.SetValue(AutomationProperties.NameProperty, "Play");
             }
         }
     }
