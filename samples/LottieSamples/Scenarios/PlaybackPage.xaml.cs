e4ca03406a868ca2983e82e4777218a25c2712ef
Marcel Wagner <marcel.alex.wagner@outlook.com>
2020-07-02 02:01:47 +0200

    CR feedback

--- samples/LottieSamples/Scenarios/PlaybackPage.xaml.cs
+++ samples/LottieSamples/Scenarios/PlaybackPage.xaml.cs
@@ -11,8 +11,8 @@ namespace LottieSamples.Scenarios
 {
     public sealed partial class PlaybackPage : Page
     {
-        private bool wasPaused = false;
-        
+        private bool _isPaused = false;
+
         public PlaybackPage()
         {
             this.InitializeComponent();
@@ -22,20 +22,55 @@ namespace LottieSamples.Scenarios
         {
             // Stop the animation, which completes PlayAsync and resets to initial frame. 
             Playback_Player.Stop();
-            Playback_Player.SetProgress(0);
             SetIsPlayingIndicator(false);
-            wasPaused = false;
+            SetDirectionIndicator(false);
+            _isPaused = false;
         }
 
-        private void ReverseButton_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Toggles the "direction" in which the animation is playing and updates the buttons icon accordingly.
+        /// The animation can either play forwards or in reverse
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DirectionButton_Click(object sender, RoutedEventArgs e)
         {
-            // Set reverse playback rate.
-            // NOTE: This property is live, which means it takes effect even if the animation is playing.
-            Playback_Player.PlaybackRate = -1;
-            StartAnimation();
+            if (ForwardArrow.Visibility == Visibility.Visible)
+            {
+                // Forward arrow was visible, that means that previous mode was reverse.
+                // Set normal playback rate.
+                // NOTE: This property is live, which means it takes effect even if the animation is playing.
+                Playback_Player.PlaybackRate = 1;
+                SetDirectionIndicator(false);
+            }
+            else
+            {
+                // Set reverse playback rate.
+                // NOTE: This property is live, which means it takes effect even if the animation is playing.
+                Playback_Player.PlaybackRate = -1;
+                SetDirectionIndicator(true);
+            }
         }
 
-        private async void StartAnimation()
+        private void SetDirectionIndicator(bool isReverse)
+        {
+            if (isReverse)
+            {
+                BackwardArrow.Visibility = Visibility.Collapsed;
+                ForwardArrow.Visibility = Visibility.Visible;
+                ToolTipService.SetToolTip(DirectionButton, "Forwards");
+                DirectionButton.SetValue(AutomationProperties.NameProperty, "Set playback direction to forwards");
+            }
+            else
+            {
+                BackwardArrow.Visibility = Visibility.Visible;
+                ForwardArrow.Visibility = Visibility.Collapsed;
+                ToolTipService.SetToolTip(DirectionButton, "Reverse");
+                DirectionButton.SetValue(AutomationProperties.NameProperty, "Set playback direction to reverse");
+            }
+        }
+
+        private async void EnsurePlaying()
         {
             if (!Playback_Player.IsPlaying)
             {
@@ -43,34 +78,48 @@ namespace LottieSamples.Scenarios
                 Playback_Player.PlaybackRate = 1;
                 SetIsPlayingIndicator(true);
                 await Playback_Player.PlayAsync(fromProgress: 0, toProgress: 1, looped: false);
-                SetIsPlayingIndicator(false);
+                if (!Playback_Player.IsPlaying)
+                {
+                    SetIsPlayingIndicator(false);
+                    SetDirectionIndicator(false);
+                }
             }
         }
 
+        /// <summary>
+        /// This handles the Play/Pause button click. There are three states in which this method can get called:
+        /// 1. Animation not playing and not started.
+        /// 2. Animation playing
+        /// 3. Animation paused
+        ///
+        /// For all of these cases, this function will either resume/start playing the animation or pause the animation and update the buttons icon accordingly
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PlayPauseButton_Click(object sender, RoutedEventArgs e)
         {
             // Is the player playing and we did not get paused?
-            if (Playback_Player.IsPlaying && !wasPaused)
+            if (Playback_Player.IsPlaying && !_isPaused)
             {
                 Playback_Player.Pause();
                 SetIsPlayingIndicator(false);
-                wasPaused = true;
+                _isPaused = true;
             }
             else
             {
-                // Either not playing or paused
-                // If paused, resume and set paused to false
-                if (wasPaused)
+                // Either not playing or paused.
+                // If paused, resume and set paused to false.
+                if (_isPaused)
                 {
                     Playback_Player.Resume();
                     SetIsPlayingIndicator(true);
                 }
-                // Not playing, start animation now
                 else
                 {
-                    StartAnimation();
+                    // Not playing, start animation now.
+                    EnsurePlaying();
                 }
-                wasPaused = false;
+                _isPaused = false;
             }
         }
 
