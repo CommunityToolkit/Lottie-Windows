8c9f44d971097de942fc9b6b162e42e0bc763e56
Simeon Cran <simeonc@microsoft.com>
2020-07-08 10:35:09 -0700

    Fix build break.

--- source/Lottie/LottieVisualSource.cs
+++ source/Lottie/LottieVisualSource.cs
@@ -407,8 +407,7 @@ namespace Microsoft.Toolkit.Uwp.UI.Lottie
                     // an issue if the author is using property bindings incorrectly.
                     translationResult = LottieToWinCompTranslator.TryTranslateLottieComposition(
                         lottieComposition: lottieComposition,
-                        options: new TranslationOptions { TranslatePropertyBindings = true, TargetUapVersion = GetCurrentUapVersion() },
-                        strictTranslation: false);
+                        configuration: new TranslatorConfiguration { TranslatePropertyBindings = true, TargetUapVersion = GetCurrentUapVersion() });
 
                     wincompDataRootVisual = translationResult.RootVisual;
                     requiredUapVersion = translationResult.MinimumRequiredUapVersion;
