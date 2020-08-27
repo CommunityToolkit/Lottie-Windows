62ac9cac6cf163eeb9a5588779fa6a0ae088d4e5
Simeon Cran <simeonc@microsoft.com>
2020-08-27 10:53:34 -0700

    UI review feedback.

--- LottieViewer/ViewModel/MarkerWithDuration.cs
+++ LottieViewer/ViewModel/MarkerWithDuration.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+
 namespace LottieViewer.ViewModel
 {
     // A marker that has a non-0 duration.
@@ -10,5 +12,7 @@ namespace LottieViewer.ViewModel
         public double ToProgress { get; set; }
 
         public string ToProgressText { get; set; }
+
+        public double ConstrainedToProgress => Math.Max(0, Math.Min(1, ToProgress));
     }
 }
