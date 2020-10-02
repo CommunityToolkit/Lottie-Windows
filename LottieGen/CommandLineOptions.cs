46ad14d4508149f5e8f1583e78edada25c7ac3d6
Simeon Cran <simeonc@microsoft.com>
2020-10-01 18:30:36 -0700

    Tweaks.

--- LottieGen/CommandLineOptions.cs
+++ LottieGen/CommandLineOptions.cs
@@ -6,7 +6,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
--- LottieGen/CommandLineTokenizer.cs
+++ LottieGen/CommandLineTokenizer.cs
@@ -6,13 +6,14 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 // Recognizes command line argument keywords such as "-help" or "-include". Matches partial
 // strings as long as they uniquely identify the keyword. Matching is case insenstive.
 sealed class CommandlineTokenizer<TKeywordId>
     where TKeywordId : struct, IComparable
 {
-    readonly TrieNode _root = new TrieNode('\0');
+    readonly TrieNode _root = new TrieNode('\0', default);
     readonly TKeywordId _ambiguousValue;
 
     internal CommandlineTokenizer(TKeywordId ambiguousValue)
@@ -35,6 +36,11 @@ sealed class CommandlineTokenizer<TKeywordId>
     // Add a keyword to the recognizer.
     internal CommandlineTokenizer<TKeywordId> AddKeyword(TKeywordId id, string keyword)
     {
+        if (IsAmbiguousId(id) || IsDefaultId(id))
+        {
+            throw new ArgumentException();
+        }
+
         TrieNode currrentNode = _root;
 
         foreach (char ch in keyword)
@@ -42,55 +48,50 @@ sealed class CommandlineTokenizer<TKeywordId>
             // Search through the current set of nodes to see if there is a node
             // for the current character already. If there is, use it, if not, add one.
             bool matched = false;
-            foreach (TrieNode n in currrentNode.Children)
+            foreach (var node in currrentNode.Children)
             {
-                if (n.Matches(ch))
+                if (node.Matches(ch))
                 {
+                    // There is already a node that matches this character.
                     matched = true;
-                    currrentNode = n;
+                    currrentNode = node;
 
-                    // Reusing an existing node. If has an id that is not the same as our
-                    // id, the string is not unique, so remove it to indicate there is
-                    // no match to this point. If the node is a terminal (i.e. it has
-                    // no children) then this keyword cannot be added as it either
-                    // includes an existing keyword, or is the same as an existing keyword.
-                    if (n.Children.Count == 0)
+                    if (node.IsKeywordMatch(id))
                     {
-                        if (!n.Keyword.HasValue)
-                        {
-                            throw new InvalidOperationException();
-                        }
-
-                        if (n.Keyword.Value.CompareTo(id) == 0)
-                        {
-                            throw new InvalidOperationException("Equivalent keyword already added.");
-                        }
-                        else
-                        {
-                            throw new InvalidOperationException("Keyword redefinition.");
-                        }
+                        throw new ArgumentException("Keyword already added.");
                     }
-                    else if (n.Keyword.HasValue && n.Keyword.Value.CompareTo(id) != 0)
+
+                    // If the node is a terminal (i.e. it has no children) then this keyword
+                    // cannot be added as it the same as an existing keyword.
+                    if (node.Children.Count == 0)
                     {
-                        n.Keyword = null;
+                        // No children, therefore this node is a terminal node.
+                        throw new ArgumentException("Keyword redefinition.");
+                    }
+                    else
+                    {
+                        // Not a terminal node. Indicate that this node matches more than
+                        // one keyword.
+                        node.Keyword = _ambiguousValue;
                     }
 
                     break;
                 }
             }
 
-            // If no match found, add a new node
+            // If no matching node found, add a new node, and set the Keyword to indicate
+            // that a match up to here matches the keyword being added.
             if (!matched)
             {
-                var newNode = new TrieNode(ch) { Keyword = id };
+                var newNode = new TrieNode(ch, id);
                 currrentNode.Children.Add(newNode);
                 currrentNode = newNode;
             }
         }
 
-        if (currrentNode.Keyword is null)
+        if (currrentNode.IsKeywordMatch(_ambiguousValue))
         {
-            throw new InvalidOperationException("Keyword matches an existing prefix.");
+            throw new ArgumentException("Keyword matches an existing prefix.");
         }
 
         return this;
@@ -111,49 +112,89 @@ sealed class CommandlineTokenizer<TKeywordId>
     // if the match was ambiguous.
     internal bool TryMatchKeyword(string keyword, out TKeywordId id)
     {
-        TrieNode currentNode = _root;
-
-        if (keyword != null)
+        if (keyword.Length == 0)
         {
-            foreach (char ch in keyword)
-            {
-                bool matched = false;
-                foreach (TrieNode n in currentNode.Children)
-                {
-                    if (n.Matches(ch))
-                    {
-                        matched = true;
-                        currentNode = n;
-                        break;
-                    }
-                }
+            id = default;
+            return false;
+        }
 
-                // If no match found, return false.
-                if (!matched)
+        var currentNode = _root;
+
+        foreach (char ch in keyword)
+        {
+            bool matched = false;
+
+            // Do a linear search of the child nodes. Linear search is appropriate
+            // because typically there are very few children.
+            foreach (var node in currentNode.Children)
+            {
+                if (node.Matches(ch))
                 {
-                    id = default;
-                    return false;
+                    matched = true;
+                    currentNode = node;
+                    break;
                 }
+            }
+
+            // If no child matched, return false.
+            if (!matched)
+            {
+                id = default;
+                return false;
             }
         }
 
-        id = currentNode.Keyword ?? (currentNode == _root ? default(TKeywordId) : _ambiguousValue);
-        return currentNode.Keyword != null;
+        // The whole of the given keyword matched.
+        id = currentNode.Keyword;
+        return !IsAmbiguousId(id);
     }
 
+    static bool IsEqual(TKeywordId a, TKeywordId b) => a.CompareTo(b) == 0;
+
+    static bool IsDefaultId(TKeywordId value) => IsEqual(value, default);
+
+    bool IsAmbiguousId(TKeywordId value) => IsEqual(value, _ambiguousValue);
+
+    /// <summary>
+    /// A node in the trie.
+    /// </summary>
     sealed class TrieNode
     {
         readonly char _character;
 
-        internal TKeywordId? Keyword { get; set; }
-
-        internal TrieNode(char character)
+        internal TrieNode(char character, TKeywordId keywordId)
         {
             _character = char.ToLower(character);
+            Keyword = keywordId;
         }
+
+        internal TKeywordId Keyword { get; set; }
+
+        internal bool IsKeywordMatch(TKeywordId keywordId) => IsEqual(Keyword, keywordId);
 
         internal List<TrieNode> Children { get; } = new List<TrieNode>();
 
         internal bool Matches(char character) => char.ToLower(character) == _character;
+
+        public override string ToString() => string.Join(", ", GetStrings());
+
+        IEnumerable<string> GetStrings()
+        {
+            var ch = _character == '\0' ? string.Empty : _character.ToString();
+            if (Children.Count == 0)
+            {
+                yield return ch;
+            }
+            else
+            {
+                foreach (var child in Children)
+                {
+                    foreach (var childString in child.GetStrings())
+                    {
+                        yield return $"{ch}{childString}";
+                    }
+                }
+            }
+        }
     }
 }
