d63112c417d2d02dd6fe0e4e7b56044ea8fce396
aborziak-ms <83784664+aborziak-ms@users.noreply.github.com>
2021-08-06 15:09:42 -0700

    Lottie Viewer | Added scale slider. Added repeating pattern background and checkbox for hiding/showing solid color bg. (#456)

--- LottieViewer/Stage.xaml.cs
+++ LottieViewer/Stage.xaml.cs
@@ -3,13 +3,22 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Numerics;
 using System.Threading.Tasks;
 using LottieViewer.ViewModel;
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.UI.Composition;
 using Microsoft.UI.Xaml.Controls;
+using Windows.Graphics.DirectX;
 using Windows.Storage;
+using Windows.Storage.Streams;
 using Windows.UI;
+using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace LottieViewer
 {
@@ -31,7 +40,54 @@ namespace LottieViewer
         {
             this.InitializeComponent();
 
-            Reset();
+            // Do not show solid background by default.
+            ShowSolidBackground = false;
+
+            // Do not show checkerboard pattern until lottie file is loaded.
+            _canvas.Visibility = Visibility.Collapsed;
+
+            _feedbackLottie.PlayInitialStateAnimation();
+        }
+
+        // Draw repeating pattern texture on backgound canvas.
+        public async Task SetupBackgroundPatternAsync()
+        {
+            var compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+            var canvasDevice = CanvasDevice.GetSharedDevice();
+            var graphicsDevice = CanvasComposition.CreateCompositionGraphicsDevice(compositor, canvasDevice);
+
+            var bitmap = await CanvasBitmap.LoadAsync(canvasDevice, @"Assets\BackgroundPattern.png");
+
+            var drawingSurface = graphicsDevice.CreateDrawingSurface(
+                bitmap.Size,
+                DirectXPixelFormat.B8G8R8A8UIntNormalized,
+                DirectXAlphaMode.Premultiplied);
+
+            using (var ds = CanvasComposition.CreateDrawingSession(drawingSurface))
+            {
+                ds.Clear(Colors.Transparent);
+                ds.DrawImage(bitmap);
+            }
+
+            var surfaceBrush = compositor.CreateSurfaceBrush(drawingSurface);
+            surfaceBrush.Stretch = CompositionStretch.None;
+
+            var border = new BorderEffect
+            {
+                ExtendX = CanvasEdgeBehavior.Wrap,
+                ExtendY = CanvasEdgeBehavior.Wrap,
+                Source = new CompositionEffectSourceParameter("source"),
+            };
+
+            var fxFactory = compositor.CreateEffectFactory(border);
+            var fxBrush = fxFactory.CreateBrush();
+            fxBrush.SetSourceParameter("source", surfaceBrush);
+
+            var sprite = compositor.CreateSpriteVisual();
+            sprite.Size = new Vector2(4096);
+            sprite.Brush = fxBrush;
+
+            ElementCompositionPreview.SetElementChildVisual(_canvas, sprite);
         }
 
         // The DiagnosticsViewModel contains information about the currently playing
@@ -40,6 +96,21 @@ namespace LottieViewer
         internal LottieVisualDiagnosticsViewModel DiagnosticsViewModel => _diagnosticsViewModel;
 
         internal AnimatedVisualPlayer Player => _player;
+
+        internal Viewbox PlayerContainer => _playerContainer;
+
+        private bool showSolidBackground = false;
+
+        internal bool ShowSolidBackground
+        {
+            get => showSolidBackground;
+
+            set
+            {
+                _backgroundColorBrush.Opacity = value ? 1.0 : 0.0;
+                showSolidBackground = value;
+            }
+        }
 
         public Color ArtboardColor
         {
@@ -56,6 +127,10 @@ namespace LottieViewer
             {
                 // Load the Lottie composition.
                 await _playerSource.SetSourceAsync(file);
+
+                await SetupBackgroundPatternAsync();
+
+                _canvas.Visibility = Visibility.Visible;
             }
             catch (Exception)
             {
@@ -88,11 +163,6 @@ namespace LottieViewer
         internal void DoDragLeave()
         {
             _feedbackLottie.PlayDragLeaveAnimation();
-        }
-
-        internal void Reset()
-        {
-            _feedbackLottie.PlayInitialStateAnimation();
         }
     }
 }
