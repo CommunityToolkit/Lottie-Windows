6be6df4e37660d643871b32aa92cc7eb7488cdc6
Andrii Borziak <aborziak@microsoft.com>
2021-08-25 15:15:54 -0700

    License added

--- LottieViewer/PixelViewElement.cs
+++ LottieViewer/PixelViewElement.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable enable
+
+using System;
 using System.Numerics;
 using System.Threading.Tasks;
 using Microsoft.Graphics.Canvas;
@@ -14,8 +20,6 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
-
-#nullable enable
 
 namespace LottieViewer
 {
