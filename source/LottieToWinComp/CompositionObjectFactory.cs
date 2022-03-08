562e07ef2215f0e782fa4c3ead4db4076b15d844
Andrii Borziak <aborziak@microsoft.com>
2022-03-08 15:27:56 -0800

    Explanation comment

--- source/LottieToWinComp/CompositionObjectFactory.cs
+++ source/LottieToWinComp/CompositionObjectFactory.cs
@@ -86,6 +86,9 @@ namespace CommunityToolkit.WinUI.Lottie.LottieToWinComp
                 case nameof(PathKeyFrameAnimation):
                     return 11;
 
+                // AnimationController class was introduced in version 6, but
+                // it became possible to create it explicitly only after verstion 14
+                // with compositor.CreateAnimationController() method
                 case nameof(AnimationController):
                     return 14;
 
